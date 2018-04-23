92de0d350a452296c2b9a686d7783dac30a48c7c
redchickSota <sota.wada1991@gmail.com>
2018-04-23 16:56:38 +1200

    setup the server

--- server/src/app.js
+++ server/src/app.js
@@ -8,11 +8,10 @@ app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/status', (req, res) => {
+app.post('/register', (req, res) => {
     res.send({
-        messgae: 'hello world!'
+        message: "Hello " + req.body.email + "! Your user was registered! Have fun!"
     })
 });
-
 
 app.listen(process.env.PORT || 8081);
