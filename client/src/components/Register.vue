79fe7cdea4fb197cc3a83c0989a5b4b8bb0a2fe2
Sota <sota.wada1991@gmail.com>
2018-05-02 16:06:59 +1200

    setting up the controllers models and config

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -5,6 +5,8 @@
     <br>
     <input type="password" name="password" v-model="password" placeholder="password" />
     <br>
+    <div class="error" v-html="error" />
+    <br>
     <button @click="register">Register</button>
   </div>
 </template>
@@ -15,16 +17,20 @@ export default {
   data () {
     return {
       email: '',
-      password: ''
+      password: '',
+      error: null
     }
   },
   methods: {
     async register () {
-      const response = await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      })
-      console.log(response.data)
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        })
+      } catch (error) {
+        this.error = error.response.data.error
+      }
     }
   }
 }
@@ -32,4 +38,7 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .error {
+    color: red;
+  }
 </style>
