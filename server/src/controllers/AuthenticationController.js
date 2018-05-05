2d6778628d9b0e9f274bb4af3954de92233d75d7
Sota <sota.wada1991@gmail.com>
2018-05-05 16:33:02 +1200

    setup login using vuex

--- server/src/controllers/AuthenticationController.js
+++ server/src/controllers/AuthenticationController.js
@@ -13,7 +13,11 @@ module.exports = {
   async register (req, res) {
     try {
       const user = await User.create(req.body)
-      res.send(user.toJSON())
+      const userJson = user.toJSON()
+      res.send({
+        user: userJson,
+        token: jwtSignUser(userJson)
+      })
     } catch (err) {
       res.status(400).send({
         error: 'This email account is already in use.'
@@ -30,14 +34,14 @@ module.exports = {
       })
       if (!user) {
         return res.status(401).send({
-          error: 'The login information was incorrect'
+          error: 'The login information was incorrect1'
         })
       }
 
       const isPasswordValid = await user.comparePassword(password)
       if (!isPasswordValid) {
         return res.status(401).send({
-          error: 'The login information was incorrect'
+          error: 'The login information was incorrect2'
         })
       }
 
